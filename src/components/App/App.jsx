cde4e666dafaba70272f5b3dfa452bff5a0e67ee
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2023-05-23 16:05:01 +0300

    change Pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 const SharedLayout = lazy(() => import('../SharedLayout/SharedLayout'));
 const Home = lazy(() => import('../../pages/Home'));
 const Contacts = lazy(() => import('../../pages/Contacts'));
-const Registration = lazy(() => import('../../pages/Login'));
-
+const Login = lazy(() => import('../../pages/Login'));
+const Registration = lazy(() => import('../../pages/Registaration'));
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="/contacts" element={<Contacts />} />
-        <Route path="/login" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
       </Route>
     </Routes>
